3fd9b58424580db503ec8ef5aa645c891383637b
Gabrielstabilee <gabrielstabilee@gmail.com>
2019-02-07 23:25:12 -0200

    atualizacoes do projeto em casa 0702

--- src/test/java/tests/informacoesUsuarioTest.java
+++ src/test/java/tests/informacoesUsuarioTest.java
@@ -1,25 +1,31 @@
 package tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.concurrent.TimeUnit;
-
 public class informacoesUsuarioTest {
-    private WebDriver navegador = new ChromeDriver();
+	private WebDriver navegador;
+	
+	@Before
+	public void SetUp() {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Gabriel\\Driver\\chromedriver.EXE");
+        navegador = new ChromeDriver();
+        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        navegador.get("http://www.juliodelima.com.br/taskit/");
+	}
+    
 
     @Test
     public void testAdicionarUmaInformacaoAdicionalDoUsuario(){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\707923\\Driver\\chromedriver.EXE");
-        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        navegador.get("http://www.juliodelima.com.br/taskit/");
-
         navegador.findElement(By.linkText("Sign in")).click();
         WebElement caixaSignIn = navegador.findElement(By.id("signinbox"));
         caixaSignIn.findElement(By.name("login")).sendKeys("julio0001");
