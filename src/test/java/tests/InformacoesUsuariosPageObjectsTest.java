0b899528e6a1cdd846fa253488dc1db4bbe7493a
707923 <Senh@151241>
2019-02-15 14:27:39 -0200

    commit

--- src/test/java/tests/InformacoesUsuariosPageObjectsTest.java
+++ src/test/java/tests/InformacoesUsuariosPageObjectsTest.java
@@ -1,12 +1,20 @@
 package tests;
 
 import Pages.LoginPage;
+import org.easetech.easytest.annotation.DataLoader;
+import org.easetech.easytest.annotation.Param;
+import org.easetech.easytest.runner.DataDrivenTestRunner;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import suporte.Web;
 
+import static org.junit.Assert.*;
+
+@RunWith(DataDrivenTestRunner.class)
+@DataLoader(filePaths = "informacoesUsuarioTestPage.csv")
 public class InformacoesUsuariosPageObjectsTest {
     private WebDriver navegador;
 
@@ -16,14 +24,17 @@ public class InformacoesUsuariosPageObjectsTest {
     }
 
     @Test
-    public void testAdicionarUmaInformacaoAdicionalDoUsuario(){
-        new LoginPage(navegador)
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario(@Param(name="login")String login, @Param(name="senha")String senha, @Param(name="tipo")String tipo, @Param(name="contato")String contato, @Param(name="mensagemEsperada")String mensagemEsperada){
+        String textoToast = new LoginPage(navegador)
                 .clickSignIn()
-                .realizarLogin("julio0001", "123456")
+                .realizarLogin(login, senha)
                 .clicarEmMe()
                 .clicarMoreDataAboutYou()
                 .clicarBotaoAddMoreData()
-        ;
+                .adicionarContato(tipo, contato)
+                .capturarTextoToast();
+
+        assertEquals(mensagemEsperada, textoToast);
     }
 
     @After
